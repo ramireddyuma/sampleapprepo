efa07d5cdb1799213458d19482bdf1a2711b2fe4
ramireddyuma <ramireddyuma2000@gmail.com>
2021-09-20 11:13:56 +0530

    array added in show

--- sample/src/main/java/sample/BootClass.java
+++ sample/src/main/java/sample/BootClass.java
@@ -4,6 +4,16 @@ public class BootClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		show();
+	}
+	
+	public static void show() {
+		int[] arr= {12,34,56,56};
+		for(int i=0;i<100;i++)
+		{
+			System.out.println(arr[i]);
+		}
+		System.out.println("show method");
 	}
 
 }
