337eadcdcd79a632fdd845e87e55d2ba4dfbfd5f
ramireddyuma <ramireddyuma2000@gmail.com>
2021-09-20 11:48:08 +0530

    sum added in show

--- sample/src/main/java/sample/BootClass.java
+++ sample/src/main/java/sample/BootClass.java
@@ -4,6 +4,16 @@ public class BootClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		show();
 	}
-
+	public static void show() {
+		int[] arr= {12,34,56,56};
+		int sum=0;
+		for(int n:arr)
+		{
+			sum+=n;
+			System.out.println(n);
+		}
+		System.out.println("show method");
+		
 }
